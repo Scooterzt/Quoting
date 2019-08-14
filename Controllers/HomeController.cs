f3c4a5f9e73772919f9e32361e9e568c863e4a82
Roman <Scooterzt@gmail.com>
2019-08-13 18:17:27 -0700

    database playground

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Quating.Models;
+
+namespace Quating.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index(){
+            //List<Dictionary<string, object>> allQuotes = DbConnector.Query("SELECT * FROM quotes");
+            return View();
+        }
+        [HttpGet("quotes")]
+        public IActionResult QuotesDisplay(){
+            List<Dictionary<string, object>> allQuotes = DbConnector.Query("SELECT * FROM quotes");
+            ViewBag.quotes = allQuotes;
+            return View("quotes");
+        }
+        [HttpPost("quotes")]
+        public IActionResult quotes(Quot q){
+            string query =$"INSERT INTO quotes (name, quote, created_at, updated_at) VALUES ('{q.Name}', '{q.Quotes}', NOW(), NOW())";
+            DbConnector.Execute(query);
+            return RedirectToAction("QuotesDisplay");
+        }
+    }
+}
---
+++ Models/Quot.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+namespace Quating.Models{
+    public class Quot{
+        public string Name {get;set;}
+        public string Quotes{get;set;}
+
+    }
+}
